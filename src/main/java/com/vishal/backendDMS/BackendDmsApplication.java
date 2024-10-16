14ee2b01b0cefd02a73c1f91d71ec0aca7369f38
vishal.yadav <vishal.yadav@qsstechnosoft.com>
2024-10-16 13:43:09 +0530

    initial commit

---
+++ src/main/java/com/vishal/backendDMS/BackendDmsApplication.java
@@ -0,0 +1,13 @@
+package com.vishal.backendDMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishal/backendDMS/BackendDmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.backendDMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
